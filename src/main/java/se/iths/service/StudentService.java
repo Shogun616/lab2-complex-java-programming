e67004e0bef667ffc89551244a491d62b30d08d8
Philip Mattsson <philip.mattsson@iths.se>
2021-11-12 13:45:36 +0100

    Added POST Controller

--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,18 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Transactional
 public class StudentService {
 
+    @PersistenceContext
+    EntityManager entityManager;
 
+    public void addStudent(Student student){
+        entityManager.persist(student);
+    }
 }
