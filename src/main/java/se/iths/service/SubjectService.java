6e7185f49e91d6118ac9a6e613395459fa1de10b
Philip Mattsson <philip.mattsson@iths.se>
2021-11-21 18:10:28 +0100

    Updated TeacherService & SubjectService

--- src/main/java/se/iths/service/SubjectService.java
+++ src/main/java/se/iths/service/SubjectService.java
@@ -1,12 +1,37 @@
 package se.iths.service;
 
+import se.iths.entity.Subject;
+
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 public class SubjectService {
 
     @PersistenceContext
     EntityManager entityManager;
+
+    public void addSubject(Subject subject){
+        entityManager.persist(subject);
+    }
+
+    public Subject findSubjectById(Long id){
+        return entityManager.find(Subject.class, id);
+    }
+
+    public void updateSubject(Subject subject){
+        entityManager.merge(subject);
+    }
+
+    public void deleteSubject(Long id){
+        Subject foundSubject = entityManager.find(Subject.class, id);
+        entityManager.remove(foundSubject);
+    }
+
+    public List<Subject> getAllSubjects(){
+        return entityManager.createQuery("select s from Subject s", Subject.class).getResultList();
+    }
 }
--- src/main/java/se/iths/service/TeacherService.java
+++ src/main/java/se/iths/service/TeacherService.java
@@ -1,12 +1,36 @@
 package se.iths.service;
 
+import se.iths.entity.Teacher;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 public class TeacherService {
 
     @PersistenceContext
     EntityManager entityManager;
+
+    public void addTeacher(Teacher teacher){
+        entityManager.persist(teacher);
+    }
+
+    public Teacher findTeacherById(Long id){
+        return entityManager.find(Teacher.class, id);
+    }
+
+    public void updateTeacher(Teacher teacher){
+        entityManager.merge(teacher);
+    }
+
+    public void deleteTeacher(Long id){
+        Teacher foundTeacher = entityManager.find(Teacher.class, id);
+        entityManager.remove(foundTeacher);
+    }
+
+    public List<Teacher> getAllTeachers(){
+        return entityManager.createQuery("select t from Teacher t", Teacher.class).getResultList();
+    }
 }
